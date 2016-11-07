cb749c3754bdd5271fb9179d48bd134a1fec9c37
aram9922 <aram_barseghyan@edu.aua.am>
2016-11-07 22:38:45 +0400

    First Commit

---
+++ public/main.js
@@ -0,0 +1,53 @@
+const deleteTodo = function(id){
+  $.ajax({
+    url: '/todoDelete',
+    type: 'delete',
+    data: JSON.stringify({itemid: id}),
+    success: (result)=>{printTodos(JSON.parse(result));}
+  });
+};
+const checkChange = function(evn){
+  $.ajax({
+    url: '/todoCheckedChange',
+    type: 'put',
+    data: JSON.stringify({itemid: $(evn).parent().attr("id")}),
+    success: (result)=>{printTodos(JSON.parse(result));}
+  });
+};
+const checked = function(bool){
+  if(bool)
+    return "checked";
+  return "";
+}
+const printTodos = function(array){
+  $("#todoList").empty();
+  for (let i=0; i<=array.length-1; i=i+1){
+    const element = '<li id="'+array[i].id+'">'+array[i].todo+'<input '+checked(array[i].checked)+' type="checkbox" onclick="checkChange(this)"/><button onclick="deleteTodo('+array[i].id+')">Delete</button></li>'
+    $("#todoList").append(element);
+  }
+};
+window.onload = function(){
+  $("#todoAddButton").on("click", function(){
+    if ($("#todoAdd").val()!=""){
+      $.ajax({
+        url: '/todoAdd',
+        type: 'post',
+        data: JSON.stringify({todo: $("#todoAdd").val(), id: null, checked: false}),
+        success: (result)=>{printTodos(JSON.parse(result));}
+      });
+      $("#todoAdd").val("");
+    }});
+    $("#todoSearchButton").on("click", function(){
+        $.ajax({
+          url: '/todoSearch',
+          type: 'post',
+          data: JSON.stringify({searchText: $("#todoSearch").val()}),
+          success: (result)=>{printTodos(JSON.parse(result));}
+        });
+      });
+  $.ajax({
+    url: '/initialTodos',
+    type: 'get',
+    success: (result)=>{printTodos(JSON.parse(result));}
+  });
+};
